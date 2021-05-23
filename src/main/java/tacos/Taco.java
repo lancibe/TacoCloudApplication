01005b45e3416715bf9cc48791d7c94fab636bdb
lancibe <1609547089@qq.com>
2021-05-23 17:04:50 +0800

    update

--- src/main/java/tacos/Taco.java
+++ src/main/java/tacos/Taco.java
@@ -4,16 +4,16 @@ import lombok.Data;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-import java.sql.Date;
+import java.util.Date;
 import java.util.List;
 
 @Data
 public class Taco {
     private Long id;
-    private Date createdA;
+    private Date createdAt;
     @NotNull
     @Size(min=5, message="Name must be ad least 5 characters long")
     private String name;
     @NotNull(message = "You must choose at least 1 ingredient")
-    private List<String> ingredients;
+    private List<Ingredient> ingredients;
 }
