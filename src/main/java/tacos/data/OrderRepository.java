22d55afbf99d2bd5e332f547c1e8a35a6236586d
lancibe <1609547089@qq.com>
2021-06-01 15:43:12 +0800

    update

--- src/main/java/tacos/data/OrderRepository.java
+++ src/main/java/tacos/data/OrderRepository.java
@@ -3,6 +3,8 @@ package tacos.data;
 import org.springframework.data.repository.CrudRepository;
 import tacos.Order;
 
+import java.util.List;
+
 public interface OrderRepository extends CrudRepository<Order, Long> {
-    Order save(Order order);
+    List<Order> findByDeliveryZip(String deliveryZip);
 }
