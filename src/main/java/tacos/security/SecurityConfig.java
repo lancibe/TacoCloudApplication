74fc03fe531f7f5b305977c4944ebe8137559991
lancibe <1609547089@qq.com>
2021-06-07 17:53:00 +0800

    update security config login

--- src/main/java/tacos/security/SecurityConfig.java
+++ src/main/java/tacos/security/SecurityConfig.java
@@ -30,6 +30,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .formLogin()
                 .loginPage("/login")
+                .usernameParameter("user")
+                .passwordParameter("pwd")
+                .defaultSuccessUrl("/design", true);
     }
 
     @Override
